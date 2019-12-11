ed69c7f2130af0560078dd9798116f2e6bc2e565
Sam Kortekaas <samkortekaas1@gmail.com>
2019-12-11 08:04:11 +0100

    first push

---
+++ printf.c
@@ -0,0 +1,12 @@
+#include <stdarg.h>
+
+int ft_printf(const char *format, ...)
+{
+	va_list args;
+
+	va_start(args, format);
+
+	
+
+	va_end(args);
+}
---
+++ printf.h
@@ -0,0 +1,10 @@
+#ifndef PRINTF_H
+# define PRINTF_H
+# include <stdlib.h>
+
+typedef struct	s_flags
+{
+
+} 				t_flags;
+
+# endif
