f86976820baf483b51186c83056314cd8e7108da
Sam Kortekaas <skorteka@f0r6s3.codam.nl>
2019-12-20 16:06:24 +0100

    Aligned scope

--- main.c
+++ main.c
@@ -3,112 +3,144 @@
 #include <stdio.h>
 #include <unistd.h>
 
+static void print_out(int i)
+{
+	ft_putnbr_fd(i, 1);
+	write(1, "\n", 1);
+}
 
 int main(void)
 {
-	ft_printf("[m: [this % i number]]\n", -267);
-	printf("[s: [this % i number]]\n", -267);
-	printf("---\n");
+	// ft_printf("[m: [this % i number]]\n", -267);
+	// printf("[s: [this % i number]]\n", -267);
+	// printf("---\n");
 
-	ft_printf("[m: [%+-5.i]]\n", 0);
-	printf("[s: [%+-5.i]]\n", 0);
-	printf("---\n");
+	// ft_printf("[m: [%+-5.i]]\n", 0);
+	// printf("[s: [%+-5.i]]\n", 0);
+	// printf("---\n");
 
-	ft_printf("[m: [%.i]]\n", 0);
-	printf("[s: [%.i]]\n", 0);
-	printf("---\n");
+	// ft_printf("[m: [%.i]]\n", 0);
+	// printf("[s: [%.i]]\n", 0);
+	// printf("---\n");
 
-	ft_printf("[m: [%+5.i]]\n", 0);
-	printf("[s: [%+5.i]]\n", 0);
-	printf("---\n");
+	// ft_printf("[m: [%+5.i]]\n", 0);
+	// printf("[s: [%+5.i]]\n", 0);
+	// printf("---\n");
 
-	ft_printf("[m: [%3i]]\n", 0);
-	printf("[s: [%3i]]\n", 0);
-	printf("---\n");
+	// ft_printf("[m: [%3i]]\n", 0);
+	// printf("[s: [%3i]]\n", 0);
+	// printf("---\n");
 
-	ft_printf("[m: [%i]\n", 5);
-	printf("[s: [%i]\n", 5);
-	printf("---\n");
+	// ft_printf("[m: [%i]\n", 5);
+	// printf("[s: [%i]\n", 5);
+	// printf("---\n");
 
-	ft_printf("[m: [%i]\n", 5);
-	printf("[s: [%i]\n", 5);
-	printf("---\n");
+	// ft_printf("[m: [%i]\n", 5);
+	// printf("[s: [%i]\n", 5);
+	// printf("---\n");
 
-	ft_printf("[m: [%.2i]]\n", -12);
-	printf("[s: [%.2i]]\n", -12);
-	printf("---\n");
+	// ft_printf("[m: [%.2i]]\n", -12);
+	// printf("[s: [%.2i]]\n", -12);
+	// printf("---\n");
 
-	ft_printf("[m: [%2i]]\n", -12);
-	printf("[s: [%2i]]\n", -12);
-	printf("---\n");
+	// ft_printf("[m: [%2i]]\n", -12);
+	// printf("[s: [%2i]]\n", -12);
+	// printf("---\n");
 
-	ft_printf("[m: [%.2i]]\n", 12);
-	printf("[s: [%.2i]]\n", 12);
-	printf("---\n");
+	// ft_printf("[m: [%.2i]]\n", 12);
+	// printf("[s: [%.2i]]\n", 12);
+	// printf("---\n");
 
-	ft_printf("[m: [%1i]]\n", 12);
-	printf("[s: [%1i]]\n", 12);
-	printf("---\n");
+	// ft_printf("[m: [%1i]]\n", 12);
+	// printf("[s: [%1i]]\n", 12);
+	// printf("---\n");
 
-	ft_printf("[m: [%1i]]\n", 12);
-	printf("[s: [%1i]]\n", 12);
-	printf("---\n");
+	// ft_printf("[m: [%1i]]\n", 12);
+	// printf("[s: [%1i]]\n", 12);
+	// printf("---\n");
 
-	ft_printf("[m: [%.4i]]\n", 12);
-	printf("[s: [%.4i]]\n", 12);
-	printf("---\n");
+	// ft_printf("[m: [%.4i]]\n", 12);
+	// printf("[s: [%.4i]]\n", 12);
+	// printf("---\n");
 
-	ft_printf("[m: [%.4i]]\n", 12);
-	printf("[s: [%.4i]]\n", 12);
-	printf("---\n");
+	// ft_printf("[m: [%.4i]]\n", 12);
+	// printf("[s: [%.4i]]\n", 12);
+	// printf("---\n");
 
-	ft_printf("[m: [%4i]]\n", 12);
-	printf("[s: [%4i]]\n", 12);
-	printf("---\n");
+	// ft_printf("[m: [%4i]]\n", 12);
+	// printf("[s: [%4i]]\n", 12);
+	// printf("---\n");
 
-	ft_printf("[m: [%4i]]\n", -12);
-	printf("[s: [%4i]]\n", -12);
-	printf("---\n");
+	// ft_printf("[m: [%4i]]\n", -12);
+	// printf("[s: [%4i]]\n", -12);
+	// printf("---\n");
 
-	ft_printf("[m: [%4.4i]]\n", 12);
-	printf("[s: [%4.4i]]\n", 12);
-	printf("---\n");
+	// ft_printf("[m: [%4.4i]]\n", 12);
+	// printf("[s: [%4.4i]]\n", 12);
+	// printf("---\n");
 
-	ft_printf("[m: [%4.4i]]\n", 12);
-	printf("[s: [%4.4i]]\n", 12);
-	printf("---\n");
+	// ft_printf("[m: [%4.4i]]\n", 12);
+	// printf("[s: [%4.4i]]\n", 12);
+	// printf("---\n");
 
-	ft_printf("[m: [%4.4i]]\n", 12);
-	printf("[s: [%4.4i]]\n", 12);
-	printf("---\n");
+	// ft_printf("[m: [%4.4i]]\n", 12);
+	// printf("[s: [%4.4i]]\n", 12);
+	// printf("---\n");
 
-	ft_printf("[m: [%4.4i]]\n", -12);
-	printf("[s: [%4.4i]]\n", -12);
-	printf("---\n");
+	// ft_printf("[m: [%4.4i]]\n", -12);
+	// printf("[s: [%4.4i]]\n", -12);
+	// printf("---\n");
 
-	ft_printf("[m: [%-3.4i]]\n", 12);
-	printf("[s: [%-3.4i]]\n", 12);
-	printf("---\n");
+	// ft_printf("[m: [%-3.4i]]\n", 12);
+	// printf("[s: [%-3.4i]]\n", 12);
+	// printf("---\n");
 
-	ft_printf("[m: [%-4.3i]]\n", 12);
-	printf("[s: [%-4.3i]]\n", 12);
-	printf("---\n");
+	// ft_printf("[m: [%-4.3i]]\n", 12);
+	// printf("[s: [%-4.3i]]\n", 12);
+	// printf("---\n");
 
-	ft_printf("[m: [%07i]]\n", -54);
-	printf("[s: [%07i]]\n", -54);
-	printf("---\n");
+	// ft_printf("[m: [%07i]]\n", -54);
+	// printf("[s: [%07i]]\n", -54);
+	// printf("---\n");
 
-	ft_printf("[m: [%8.5i]]\n", 34);
-	printf("[s: [%8.5i]]\n", 34);
-	printf("---\n");
+	// ft_printf("[m: [%8.5i]]\n", 34);
+	// printf("[s: [%8.5i]]\n", 34);
+	// printf("---\n");
 
-	ft_printf("[m: [%05i]]\n", 234);
-	printf("[s: [%05i]]\n", 234);
-	printf("---\n");
+	// ft_printf("[m: [%05i]]\n", 234);
+	// printf("[s: [%05i]]\n", 234);
+	// printf("---\n");
 
-	ft_printf("[m: [%10.5i]]\n", -216);
-	printf("[s: [%10.5i]]\n", -216);
-	printf("---\n");
+	// ft_printf("[m: [%10.5i]]\n", -216);
+	// printf("[s: [%10.5i]]\n", -216);
+	// printf("---\n");
+
+	print_out(ft_printf("[m: [this % i number]]\n", -267));
+	print_out(ft_printf("[m: [%+-5.i]]\n", 0));
+	print_out(ft_printf("[m: [%.i]]\n", 0));
+	print_out(ft_printf("[m: [%+5.i]]\n", 0));
+	print_out(ft_printf("[m: [%3i]]\n", 0));
+	print_out(ft_printf("[m: [%i]\n", 5));
+	print_out(ft_printf("[m: [%i]\n", 5));
+	print_out(ft_printf("[m: [%.2i]]\n", -12));
+	print_out(ft_printf("[m: [%2i]]\n", -12));
+	print_out(ft_printf("[m: [%.2i]]\n", 12));
+	print_out(ft_printf("[m: [%1i]]\n", 12));
+	print_out(ft_printf("[m: [%1i]]\n", 12));
+	print_out(ft_printf("[m: [%.4i]]\n", 12));
+	print_out(ft_printf("[m: [%.4i]]\n", 12));
+	print_out(ft_printf("[m: [%4i]]\n", 12));
+	print_out(ft_printf("[m: [%4i]]\n", -12));
+	print_out(ft_printf("[m: [%4.4i]]\n", 12));
+	print_out(ft_printf("[m: [%4.4i]]\n", 12));
+	print_out(ft_printf("[m: [%4.4i]]\n", 12));
+	print_out(ft_printf("[m: [%4.4i]]\n", -12));
+	print_out(ft_printf("[m: [%-3.4i]]\n", 12));
+	print_out(ft_printf("[m: [%-4.3i]]\n", 12));
+	print_out(ft_printf("[m: [%07i]]\n", -54));
+	print_out(ft_printf("[m: [%8.5i]]\n", 34));
+	print_out(ft_printf("[m: [%05i]]\n", 234));
+	print_out(ft_printf("[m: [%10.5i]]\n", -216));
 
 	return (0);
 }
