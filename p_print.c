aba290f73c7894961320e6616291880c9919b8bc
Sam Kortekaas <samkortekaas1@gmail.com>
2019-12-31 10:15:24 +0100

    Now passing pft

--- p_print.c
+++ p_print.c
@@ -21,20 +21,17 @@ static t_length	get_length(t_flags flags, unsigned long long num)
 	char *str;
 
 	str = ft_itoa_base(num, 16);
-	len.numlen = ((flags.prec == 0 && num == 0) ? 0 : ft_strlen(str));
-	len.p_padlen = ((flags.prec != -1 && flags.prec >= len.numlen) ?
-					flags.prec - len.numlen : 0);
-	len.p_numlen = len.numlen + len.p_padlen;
-	len.t_numlen = ((len.sign == '-' || (len.sign == '+' && flags.plus == 1)
-					|| flags.space == 1) ? len.p_numlen + 1 : len.p_numlen);
-	len.w_padlen = ((flags.width != -1 && flags.width >= len.t_numlen) ?
-					flags.width - len.t_numlen : 0);
-	len.total_len = len.t_numlen + len.w_padlen;	
+	len.t_numlen = ft_strlen(str);
+	len.w_padlen = (flags.width - len.t_numlen - 2 < 0 ? 0 : flags.width - len.t_numlen - 2);
+	len.total_len = len.w_padlen + len.t_numlen + 2;
+		
 	return (len);
 }
 
 static void		fill_width(t_flags flags, t_length len, int *i, char **str)
 {
+	int extra_for_hash;
+
 	*i = 0;
 	if (flags.minus == -1)
 	{
@@ -42,7 +39,11 @@ static void		fill_width(t_flags flags, t_length len, int *i, char **str)
 		(*i) += len.w_padlen;
 	}
 	if (flags.minus == 1)
-		ft_memset(((*str) + len.t_numlen), flags.zero, len.w_padlen);
+	{
+		extra_for_hash = 2;
+		ft_memset(((*str) + extra_for_hash + len.t_numlen), flags.zero, len.w_padlen);
+	}
+		
 }
 
 static char		*create_string(t_flags flags, t_length len, unsigned long long num)
@@ -56,10 +57,15 @@ static char		*create_string(t_flags flags, t_length len, unsigned long long num)
 	if (!str)
 		return (NULL);
 	num_str = ft_itoa_base(num, 16);
-	flags.zero = ((flags.prec == -1 && flags.zero == 1) ? '0' : ' ');
+	flags.zero = ' ';
 	fill_width(flags, len, &i, &str);
-	ft_memset((str + i), '0', len.p_padlen);
-	i += len.p_padlen;
+	if (flags.zero == ' ')
+	{
+		str[i] = '0';
+		i++;
+		str[i] = 'x';
+		i++;
+	}
 	j = 0;
 	while (num_str[j] && flags.prec != 0)
 	{
@@ -73,17 +79,36 @@ static char		*create_string(t_flags flags, t_length len, unsigned long long num)
 	return (str);
 }
 
+static char *set_case(char *str, t_flags flags)
+{
+	int i;
+
+	i = 0;
+	while (str[i] && flags.conv == 'X')
+	{
+		str[i] = ft_toupper(str[i]);
+		i++;
+	}
+	while (str[i] && flags.conv == 'x')
+	{
+		str[i] = ft_tolower(str[i]);
+		i++;
+	}
+	return (str);
+}
+
 int				p_print(va_list args, t_flags flags)
 {
-	unsigned long long		num;
-	t_length	len;
-	char		*str;
+	unsigned long long	num;
+	t_length		len;
+	char			*str;
 	
-	num = (unsigned long long)va_arg(args, long long);
+	num = (unsigned long long)va_arg(args, void*);
 	len = get_length(flags, num);
 	str = create_string(flags, len, num);
 	if (!str)
 		return (0);
+	str = set_case(str, flags);
 	write(1, str, ft_strlen(str));
 	if (str)
 		free(str);
