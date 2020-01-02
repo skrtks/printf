2af2bc25865ec6bcde99d149b790956b1f645627
Sam Kortekaas <skorteka@f0r6s8.codam.nl>
2020-01-02 11:44:12 +0100

    Updated for norme

--- printf.h
+++ printf.h
@@ -17,40 +17,39 @@
 
 typedef struct	s_flags
 {
-	int minus;
-	int zero;
-	int hash;
-	int apo;
-	int space;
-	int plus;
-	int width;
-	int prec;
-	char conv;
-} 				t_flags;
+	int			minus;
+	int			zero;
+	int			hash;
+	int			apo;
+	int			space;
+	int			plus;
+	int			width;
+	int			prec;
+	char		conv;
+}				t_flags;
 
-typedef struct s_length {
-	int numlen;
-	int p_numlen;
-	int t_numlen;
-	int p_padlen;
-	int w_padlen;
-	int total_len;
-	char sign;
-}		t_length;
+typedef struct	s_length {
+	int			numlen;
+	int			p_numlen;
+	int			t_numlen;
+	int			p_padlen;
+	int			w_padlen;
+	int			total_len;
+	char		sign;
+}				t_length;
 
-int ft_printf(const char *format, ...);
-int	c_print(va_list args, t_flags flags);
-int	s_print(va_list args, t_flags flags);
-int	i_print(va_list args, t_flags flags);
-int	x_print(va_list args, t_flags flags);
-int	p_print(va_list args, t_flags flags);
+int				ft_printf(const char *format, ...);
+int				c_print(va_list args, t_flags flags);
+int				s_print(va_list args, t_flags flags);
+int				i_print(va_list args, t_flags flags);
+int				x_print(va_list args, t_flags flags);
+int				p_print(va_list args, t_flags flags);
 
-int		int_length(long n);
-char	*ft_itoa_base(long long value, int base);
-char	*ft_itoa_uns(int n);
-int		u_print(va_list args, t_flags flags);
-char	*set_case(char *str, t_flags flags);
-int		set_string(char **dst, char *src, int i);
+int				int_length(long n);
+char			*ft_itoa_base(long long value, int base);
+char			*ft_itoa_uns(int n);
+int				u_print(va_list args, t_flags flags);
+char			*set_case(char *str, t_flags flags);
+int				set_string(char **dst, char *src, int i);
 
-
-# endif
+#endif
